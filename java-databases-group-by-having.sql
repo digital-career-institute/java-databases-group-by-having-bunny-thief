2f0991a01ee55a61b6228547224404800cde22f2
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-01-29 14:59:23 +0100

    Add solution to exercise #3

--- java-databases-group-by-having.sql
+++ java-databases-group-by-having.sql
@@ -1,4 +1,4 @@
--- Java- Databases: Group By & Having
+--- Java- Databases: Group By & Having
 	
 
 --  1. Select all orders that have total price greater than 300.
@@ -8,8 +8,17 @@
 	ORDER BY order_id;
 
 --  2. Select only employees that have more than 100 orders.
+	SELECT e.employee_id, e.first_name, e.last_name, FROM employees as e
+	JOIN orders ON orders.employee_id = e.employee_id
+	JOIN order_details ON order_details.order_id = orders.order_id
+	GROUP BY e.employee_id
+	HAVING (SELECT * FROM order_details );
 
 --  3. Select the latest order (order_id, customer_id, order_date) for each customer. Include only orders that were made after 1998-05-05.
+	SELECT order_id, customer_id, order_date FROM orders
+	WHERE order_date > '1998-05-05'
+	GROUP BY order_id
+	ORDER BY order_date DESC;
 
 --  4. Select shipper company name and calculate how many orders have each.
 
